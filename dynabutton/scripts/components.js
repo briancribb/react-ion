299e67dd20563338b43c917098e3430a9e5004b6
Brian Cribb <brian@briancribb.com>
2017-06-16 13:17:00 -0400

    Button done.

--- dynabutton/scripts/components.js
+++ dynabutton/scripts/components.js
@@ -7,53 +7,68 @@ class DynaButton extends React.Component {
 				{
 					name: 'First Thing',
 					desc: 'This is the description for the first thing and so on and so forth.',
-					value: 'test'
+					value: 'thing01'
 				},
 				{
 					name: 'Second Thing',
 					desc: 'The second thing has this description.',
-					value: 'test'
+					value: 'thing02'
 				},
 				{
 					name: 'Third Thing',
 					desc: 'We have another thing here.',
-					value: 'test'
+					value: 'thing03'
 				}
 			],
 			stuffs = [
 				{
 					name: 'Some Stuff',
 					desc: 'Stuff happens, and this is some of it.',
-					value: 'test'
+					value: 'stuff01'
 				},
 				{
 					name: 'Other Stuff',
 					desc: 'Here is the other stuff.',
-					value: 'test'
+					value: 'stuff02'
+				},
+				{
+					name: 'Even More Stuff',
+					desc: 'Holy cow, we really do have even more stuff.',
+					value: 'stuff03'
 				}
 			],
 			doodads = [
 				{
 					name: 'Doodad One',
 					desc: 'Doodads be doodads, yo.',
-					value: 'test'
+					value: 'doodad01'
 				},
 				{
 					name: 'Another Doodad',
 					desc: 'More description for doodads.',
-					value: 'test'
+					value: 'doodad02'
+				},
+				{
+					name: 'A Third Doodad',
+					desc: 'This is too much information about doodads.',
+					value: 'doodad03'
 				}
 			],
 			dinglehoppers = [
 				{
 					name: 'Dinglehopper',
 					desc: 'This is the description for the first thing.',
-					value: 'test'
+					value: 'dinglehopper01'
 				},
 				{
 					name: 'Snarfblat',
 					desc: 'This is nothing like a dinglehopper at all.',
-					value: 'test'
+					value: 'dinglehopper02'
+				},
+				{
+					name: "Here's another Dinglehopper!",
+					desc: 'An actual dinglehopper, which is totally legit.',
+					value: 'dinglehopper03'
 				}
 			];
 
@@ -71,19 +86,12 @@ class DynaButton extends React.Component {
 			dinglehopper:0
 		}
 
+		//let that = this;
+		//window.getState = function() {
+		//	return that.state;
+		//}
 	}
-	componentDidMount() {
-		this._getData();
-	}
-	_getData() {
-		console.log('_getData()');
 
-		var obj = {
-			initialized:true
-		}
-		this.setState(obj);
-
-	}
 	_updateTopState(obj) {
 		this.setState(obj);
 	}
@@ -104,21 +112,14 @@ class DynaButton extends React.Component {
 		var obj = {};
 		obj[arrayIndex] = index;
 
-
-
-
-
 		//console.log(obj);
 		this.setState(obj);
-
-		console.log([strArrayName, arrayIndex, direction, this.state[arrayIndex] ]);
-		console.log(' ===== ');
 	}
 
 	/*
 	componentDidMount() {
 		console.log('componentDidMount()');
-	}
+	}å
 	*/
 
 	/*
@@ -130,66 +131,47 @@ class DynaButton extends React.Component {
 
 	render() {
 		let markup = null;
-		if (this.state.initialized) {
-			markup = 
-				/*
-				<form>
-					<ButtonGroup type="primary" updateTopState={this._updateTopState.bind(this)} />
-					<div className="form-group">
-						<button type="submit" className="btn btn-primary"
-							data-place={this.state.place} 
-						>{this.state.place}</button>
-					</div>
-				</form>
-				*/
-				<div id="steps">
-					<h4>Navigate through the things:</h4>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a purus orci. Fusce ultricies turpis eu velit faucibus hendrerit. Nulla egestas urna ac sapien varius efficitur.</p>
-
-
-
-
-
-					<ButtonGroup array="things" arrayIndex="thing" current={this.state.data.things[this.state.thing]} prevNext={this._prevNext.bind(this)} />
-
-
-
-
-
-					<h4>Search through the stuff:</h4>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a purus orci. Fusce ultricies turpis eu velit faucibus hendrerit. Nulla egestas urna ac sapien varius efficitur.</p>
-					<div className="btn-group btn-group-lg mb-2" role="group" aria-label="Basic example">
-						<button type="button" className="btn btn-secondary"><i className="fa fa-lg fa-chevron-circle-left" aria-hidden="true"></i></button>
-						<button type="button" className="btn btn-secondary"><i className="fa fa-lg fa-chevron-circle-right" aria-hidden="true"></i></button>
-					</div>
-					<p className="mb-4"><strong>Item 1</strong>: This is serious stuff</p>
-
-					<h4>Find the Doodad:</h4>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a purus orci. Fusce ultricies turpis eu velit faucibus hendrerit. Nulla egestas urna ac sapien varius efficitur.</p>
-					<div className="btn-group btn-group-lg mb-2" role="group" aria-label="Basic example">
-						<button type="button" className="btn btn-secondary"><i className="fa fa-lg fa-chevron-circle-left" aria-hidden="true"></i></button>
-						<button type="button" className="btn btn-secondary"><i className="fa fa-lg fa-chevron-circle-right" aria-hidden="true"></i></button>
-					</div>
-					<p className="mb-4"><strong>Doodad 1</strong>: basically a thingamajig</p>
-					
-					<h4>Look through the Dinglehoppers</h4>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a purus orci. Fusce ultricies turpis eu velit faucibus hendrerit. Nulla egestas urna ac sapien varius efficitur.</p>
-					<div className="btn-group btn-group-lg mb-2" role="group" aria-label="Basic example">
-						<button type="button" className="btn btn-secondary"><i className="fa fa-lg fa-chevron-circle-left" aria-hidden="true"></i></button>
-						<button type="button" className="btn btn-secondary"><i className="fa fa-lg fa-chevron-circle-right" aria-hidden="true"></i></button>
-					</div>
-					<p className="mb-4"><strong>Dinglehopper 1</strong>: Nothing like a snarfblat at all.</p>
-
-
-
-					<h4>The Final Button</h4>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a purus orci. Fusce ultricies turpis eu velit faucibus hendrerit. Nulla egestas urna ac sapien varius efficitur.</p>
-					<div className="btn-group btn-group-lg mb-4 center-block" role="group" aria-label="Basic example">
-						<button type="button" className="btn btn-primary">Submit</button>
-					</div>
+		markup = 
+			/*
+			<form>
+				<ButtonGroup type="primary" updateTopState={this._updateTopState.bind(this)} />
+				<div className="form-group">
+					<button type="submit" className="btn btn-primary"
+						data-place={this.state.place} 
+					>{this.state.place}</button>
 				</div>
-			;
-		}
+			</form>
+			*/
+			<div id="steps">
+				<h4>Navigate through the things:</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a purus orci. Fusce ultricies turpis eu velit faucibus hendrerit. Nulla egestas urna ac sapien varius efficitur.</p>
+				<ButtonGroup array="things" arrayIndex="thing" current={this.state.data.things[this.state.thing]} prevNext={this._prevNext.bind(this)} />
+
+
+				<h4>Search through the stuff:</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a purus orci. Fusce ultricies turpis eu velit faucibus hendrerit. Nulla egestas urna ac sapien varius efficitur.</p>
+				<ButtonGroup array="stuffs" arrayIndex="stuff" current={this.state.data.stuffs[this.state.stuff]} prevNext={this._prevNext.bind(this)} />
+
+
+				<h4>Find the Doodad:</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a purus orci. Fusce ultricies turpis eu velit faucibus hendrerit. Nulla egestas urna ac sapien varius efficitur.</p>
+				<ButtonGroup array="doodads" arrayIndex="doodad" current={this.state.data.doodads[this.state.doodad]} prevNext={this._prevNext.bind(this)} />
+
+
+				<h4>Look through the Dinglehoppers</h4>
+				<ButtonGroup array="dinglehoppers" arrayIndex="dinglehopper" current={this.state.data.dinglehoppers[this.state.dinglehopper]} prevNext={this._prevNext.bind(this)} />
+
+				<h4>The Final Button</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a purus orci. Fusce ultricies turpis eu velit faucibus hendrerit. Nulla egestas urna ac sapien varius efficitur.</p>
+				<div className="btn-group btn-group-lg mb-4 center-block" role="group" aria-label="Basic example">
+					<SubmitButton
+						thing={this.state.data.things[this.state.thing].value} 
+						stuff={this.state.data.stuffs[this.state.stuff].value} 
+						doodad={this.state.data.doodads[this.state.doodad].value} 
+						dinglehopper={this.state.data.dinglehoppers[this.state.dinglehopper].value} />
+				</div>
+			</div>
+		;
 		return(
 			markup
 		); 
@@ -207,8 +189,6 @@ class ButtonGroup extends React.Component {
 	}
 
 	render() {
-		//console.log('this.props');
-		//console.log(this.props);
 		return(
 				<div className="form-group">
 					<div className="btn-group btn-group-lg mb-2" role="group" aria-label="Basic example">
@@ -236,13 +216,20 @@ class ButtonGroup extends React.Component {
 }
 */
 
-class PlaceButton extends React.Component {
+class SubmitButton extends React.Component {
 	_handleClick() {
-		this.props.updateTopState({place:this.props.place});
+		console.log("Clicked the thing!");
 	}
 	render() {
 		return(
-				<button onClick={this._handleClick.bind(this)} type="button" className={"btn btn-" + this.props.type}>{this.props.place}</button>
+				<button onClick={this._handleClick.bind(this)} 
+				type="button" 
+				className="btn btn-primary"
+				data-thing={this.props.thing} 
+				data-stuff={this.props.stuff} 
+				data-doodad={this.props.doodad} 
+				data-dinglehopper={this.props.dinglehopper} 
+				>Submit</button>
 			);
 	}
 }
